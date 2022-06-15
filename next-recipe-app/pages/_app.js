eb12864825c4f4cea321eb0ba7e1922ff9fee344
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2022-06-15 11:14:05 -0400

    Update _app.js
    making changes to app.js

--- next-recipe-app/pages/_app.js
+++ next-recipe-app/pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+   <nav>
+     <div>
+      <Link href="/">
+        <a>Kap's Kitchen</a>
+      </Link>
+     </div>
+   </nav>
+     <main>
+      <Component {...pageProps} />
+     </main>
+  </>
+  
+  )
 }
 
 export default MyApp
